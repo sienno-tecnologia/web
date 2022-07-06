ca32c80f6c8e508f210a9d6ec18667c57fdfc420
Aluno <aluno@embarquedigital.portodigital.local>
2022-07-06 17:58:35 -0300

    carousel

--- src/main.js
+++ src/main.js
@@ -11,3 +11,121 @@ function handleToggleMenu() {
 openMenuButton.addEventListener('click', handleToggleMenu)
 closeMenuButton.addEventListener('click', handleToggleMenu)
 overlay.addEventListener('click', handleToggleMenu)
+
+let currentCarouselItem = 0
+// CAROUSEL ----
+
+//
+
+const members = [
+  {
+    name: 'Miguel Costa',
+    role: 'Programador',
+    bio: 'deixa',
+  },
+  {
+    name: 'Sabrina Ferreira',
+    role: 'Designer',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+  {
+    name: 'Maria Rito',
+    role: 'Designer',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+  {
+    name: 'Alexsandro Chaves',
+    role: 'Pesquisador',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+  {
+    name: 'Gustavo Souza',
+    role: 'Pesquisador',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+  {
+    name: 'Beatriz Oliveira',
+    role: 'Pesquisadora',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+  {
+    name: 'Pedro Bezerra',
+    role: 'Gestor',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+  {
+    name: 'Vinicius Santos',
+    role: 'Programador',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+  {
+    name: 'Ayrton Demétrio',
+    role: 'Designer',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+  {
+    name: 'Gabriel Gomes',
+    role: 'Programador',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed porro similique assumenda illum nostrum sequi corrupti ipsa enim.',
+  },
+]
+
+function createCarouselTemplate(name, bio, role) {
+  const parent = document.createElement('div')
+
+  parent.className = 'carousel-item'
+  parent.id = 'carousel-item'
+
+  const template = `
+      <div class="picture"></div>
+        <div class="content">
+          <h1>${name}</h1>
+          <h2 class="emphasis">${role}</h2>
+          <div class="icons">
+            <img src="./assets/github.webp" alt="github" />
+            <img src="./assets/linkedin.webp" alt="linkedin" />
+          </div>
+          <div class="github"></div>
+          <div class="linkedin"></div>
+          <p>
+              ${bio}
+          </p>
+        </div>
+  `
+
+  parent.innerHTML = template
+
+  return parent
+}
+
+const scrollRightBtn = document.getElementById('scroll-right-btn')
+const scrollLeftButton = document.getElementById('scroll-left-btn')
+const carouselContainer = document.getElementById('carousel')
+
+scrollRightBtn.addEventListener('click', () => {
+  const carouselItem = document.getElementById('carousel-item')
+  carouselContainer.scrollLeft += carouselItem.offsetWidth
+
+  if (currentCarouselItem < members.length) currentCarouselItem++
+
+  console.log(currentCarouselItem)
+})
+
+scrollLeftButton.addEventListener('click', () => {
+  const carouselItem = document.getElementById('carousel-item')
+  carouselContainer.scrollLeft -= carouselItem.offsetWidth
+
+  if (currentCarouselItem > 0) currentCarouselItem--
+
+  console.log(currentCarouselItem)
+})
+;(() => {
+  members.forEach(member => {
+    const template = createCarouselTemplate(
+      member.name,
+      member.bio,
+      member.role
+    )
+    carouselContainer.appendChild(template)
+  })
+})()
